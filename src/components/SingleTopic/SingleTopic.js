5ccb630ed61138bc1b7e8cac3f42e1c4c5deea02
ShawnRong <rongshawn@gmail.com>
2018-02-20 16:13:59 +0800

    Add Topic List Style

---
+++ src/components/SingleTopic/SingleTopic.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import moment from "moment";
+import "moment/locale/zh-cn";
+
+const singleTopic = props => {
+  let avatarImg = "https:" + props.topicAuthor.avatar_normal;
+  return (
+    <View style={styles.topicItem}>
+      <View style={styles.topicAvatarArea}>
+        <Image source={{ uri: avatarImg }} style={styles.topicAvatar} />
+      </View>
+      <View style={styles.topicInfoArea}>
+        <Text>{props.topicTitle}</Text>
+        <View style={styles.topicDetailArea}>
+          <View style={styles.topicDetailTag}>
+            <Text style={styles.topicDetailText}>{props.topicNode.title}</Text>
+          </View>
+          <View style={styles.topicDetailUser}>
+            <Text style={[{ fontWeight: "bold" }, styles.topicDetailText]}>
+              {props.topicAuthor.username}
+            </Text>
+          </View>
+          <View>
+            <Text style={styles.topicDetailText}>
+              {moment
+                .unix(props.topicLastModify)
+                .startOf("hour")
+                .fromNow()}
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.topicReplyCountArea}>
+        <Text style={{ color: "#fff", fontWeight: "bold" }}>
+          {props.topicReplies}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  topicItem: {
+    width: "100%",
+    padding: 10,
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderColor: "#e2e2e2",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  topicAvatar: {
+    height: 30,
+    width: 30
+  },
+  topicAvatarArea: {
+    width: "12%"
+  },
+  topicInfoArea: {
+    width: "80%"
+  },
+  topicReplyCountArea: {
+    width: "8%",
+    backgroundColor: "#aab0c6",
+    borderRadius: 10,
+    alignItems: "center"
+  },
+  topicDetailArea: {
+    flexDirection: "row",
+    width: "100%",
+    marginTop: 5,
+    alignItems: "center"
+  },
+  topicDetailTag: {
+    marginRight: 10,
+    backgroundColor: "#f5f5f5"
+  },
+  topicDetailUser: {
+    marginRight: 10
+  },
+  topicDetailText: {
+    fontSize: 12,
+    color: "#999"
+  }
+});
+
+export default singleTopic;
--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,14 +1,74 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Dimensions, StyleSheet } from "react-native";
+import { TabViewAnimated, TabBar, SceneMap } from "react-native-tab-view";
+
+import TopicsList from "../../components/TopicsList/TopicsList";
+
+const initialLayout = {
+  height: 0,
+  width: Dimensions.get("window").width
+};
+
+const FirstRoute = TopicsList;
+
+const SecondRoute = () => (
+  <View style={[styles.container, { backgroundColor: "#673ab7" }]} />
+);
 
 class Main extends Component {
+  state = {
+    index: 0,
+    routes: [{ key: "hot", title: "最热" }, { key: "all", title: "全部" }]
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderHeader = props => (
+    <TabBar
+      style={styles.tabBarStyle}
+      tabStyle={styles.tabBarItemStyle}
+      labelStyle={styles.tabBarLabelStyle}
+      indicatorStyle={styles.tabBarItemActiveStyle}
+      {...props}
+    />
+  );
+
+  _renderScene = SceneMap({
+    hot: FirstRoute,
+    all: SecondRoute
+  });
+
   render() {
     return (
-      <View>
-        <Text>This is main screen</Text>
-      </View>
+      <TabViewAnimated
+        style={styles.container}
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderHeader={this._renderHeader}
+        onIndexChange={this._handleIndexChange}
+        initialLayout={initialLayout}
+      />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  tabBarStyle: {
+    height: 30,
+    backgroundColor: "#fff"
+  },
+  tabBarItemStyle: {
+    padding: 0
+  },
+  tabBarItemActiveStyle: {
+    backgroundColor: "#445"
+  },
+  tabBarLabelStyle: {
+    color: "#000"
+  }
+});
 
 export default Main;
